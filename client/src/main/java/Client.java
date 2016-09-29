c27d73080ac705bdcd3ec9e932bad4d8cad635f9
Tim Miller <t.r.miller0823@gmail.com>
2016-09-29 17:16:03 -0400

    Tracker now accepts connections, and has a simple interface for viewing the tracker status

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,4 +1,166 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Logger;
 
-public class Client {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
 
+/*
+ * Simple logger, with 3 levels, and it prints the thread that's invoking it
+ */
+class ThreadLogger {
+	static enum MsgType {
+		ERROR, NORMAL, WARN;
+	}
+
+	public final InputStream	in;
+
+	public final PrintStream	out;
+
+	public ThreadLogger(InputStream in, OutputStream out) {
+		this(in, new PrintStream(out));
+	}
+
+	public ThreadLogger(InputStream in, PrintStream out) {
+		this.in = in;
+		this.out = out;
+	}
+
+	public void error(String msg) {
+		println(MsgType.ERROR, msg);
+	}
+
+	public void log(String msg) {
+		println(MsgType.NORMAL, msg);
+	}
+
+	public void println(MsgType type, String msg) {
+		out.println("[" + Thread.currentThread().getName() + "] [" + type.name() + "] " + msg);
+	}
+
+	public void warn(String msg) {
+		println(MsgType.WARN, msg);
+	}
+}
+
+/*
+ * connects to a given tracker, sends the port it's listening on, then waits for a message.  After it gets the message, it parses the message to a remote client and connects, and then the clients communicate directly
+ */
+public class Client implements AutoCloseable, Runnable {
+	
+	public final ThreadLogger logger = new ThreadLogger(System.in, System.out);
+	
+	private boolean running = true;
+	public boolean isRunning(){
+		return running;
+	}
+	
+	protected synchronized void stop(){
+		if(running)
+			running = false;
+		else
+			throw new RuntimeException("Client already stopped");
+	}
+	protected final ServerSocket	server;
+	
+	public class ConnectThread extends Thread {
+		protected final Tracker			tracker;
+
+		public ConnectThread(String host, int port) {
+			tracker = new Tracker(host, port);
+		}
+
+		@Override
+		public void run() {
+			// send port we're listening to to tracker
+			try {
+				logger.log("Starting to send the localport");
+				tracker.out.writeBytes(server.getLocalPort() + "\n");
+				logger.log("Sent localport");
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	protected class Tracker implements AutoCloseable {
+		public final BufferedReader		in;
+
+		public final DataOutputStream	out;
+		private Socket					sock;
+		@Expose
+		public final int				port;
+
+		@Expose
+		public final String				host;
+
+		public Tracker(String host, int port) {
+			this.host = host;
+			this.port = port;
+			try {
+				sock = new Socket(host, port);
+				in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+				out = new DataOutputStream(sock.getOutputStream());
+			} catch (IOException ioe) {
+				throw new RuntimeException("Error connecting to tracker " + toString(), ioe);
+			}
+		}
+		
+		public void close(){
+			try {
+				sock.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public String toString() {
+			return gson.toJson(this, Tracker.class);
+		}
+	}
+
+	public static final Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+
+	public static void main(String... args) {
+		try (Client client = new Client()) {
+			client.run();
+		}
+	}
+
+	public Client() {
+		try {
+			// find open port
+			server = new ServerSocket(0);
+		} catch (IOException ioe) {
+			throw new RuntimeException("Error opening port", ioe);
+		}
+	}
+
+	@Override
+	public void close() {
+		try {
+			this.server.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected ConnectThread connectThread;
+	
+	@Override
+	public void run() {
+		// start the ConnectThread
+		// TODO rewrite to use a config file
+		connectThread = new ConnectThread("localhost", 8000);
+		connectThread.start();
+	}
 }
--- tracker/src/main/java/Tracker.java
+++ tracker/src/main/java/Tracker.java
@@ -1,16 +1,22 @@
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.function.Supplier;
@@ -18,11 +24,6 @@ import java.util.function.Supplier;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
-
-@FunctionalInterface
-interface TriFunction<A, B, C, R>{
-	public R apply(A a, B b, C c);
-}
 
 /*
  * Tracker for p2p chat system. It listens on a given port (8000 by default) for
@@ -35,11 +36,78 @@ public class Tracker implements AutoCloseable {
 	 * Configuration for a tracker, typically loaded from a .json file
 	 */
 	public static class Config {
+		public static Config DEFAULT = new Config();
+		static {
+			DEFAULT.port = 8000;
+		}
+
 		/*
 		 * The port to listen for
 		 */
 		@Expose
 		public int port;
+	}
+
+	protected class ListenThread extends Thread {
+		public ListenThread() {
+			setName("ListenThread");
+		}
+
+		@Override
+		public void run() {
+			Socket clientSocket;
+			logger.log("Starting to listen...");
+			while (isRunning()) {
+				try {
+					clientSocket = socket.accept();
+					logger.log("Got connection!");
+				} catch (SocketException se) {
+					if (se.getMessage().equals("Socket closed")) {
+						if (isRunning())
+							throw new RuntimeException(se);
+						else {
+							break;
+						}
+					} else {
+						continue;
+					}
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+					continue;
+				}
+				Thread thread = new ClientConnectionThread(new ClientConnection(clientSocket));
+				thread.start();
+			}
+		}
+	}
+	protected class MatchThread extends Thread {
+		int pairs = 0;
+
+		public MatchThread() {
+			setName("MatchThread");
+		}
+
+		public Thread pairThread(ClientConnection conn1, ClientConnection conn2) {
+			
+			return new Thread(() -> {
+				logger.log("Starting to pair...");
+				// TODO send the data for the two clients to each other
+				// NOTE this single thread sends conn1 to conn2 and visa versa
+			}, "PairThread#" + (pairs++));
+		}
+
+		@Override
+		public void run() {
+			logger.log("Starting to match...");
+			ClientConnection cc1, cc2;
+			while (isRunning()) {
+				if (clients.size() >= 2) {
+					cc1 = clients.pop();
+					cc2 = clients.pop();
+					pairThread(cc1, cc2).start();
+				}
+			}
+		}
 	}
 
 	/*
@@ -48,22 +116,22 @@ public class Tracker implements AutoCloseable {
 	 * docstring, which if used provides a brief description of the command
 	 */
 	protected class Task {
-		public final String		docstring;
+		public final String				docstring;
 		private final Supplier<String>	supplier;
-
-		public Task(Supplier<String> fn) {
-			this(null, fn);
-		}
 
 		public Task(String doc, Supplier<String> fn) {
 			if (doc == null) {
 				doc = "No doc";
 			}
-			this.docstring = doc;
+			docstring = doc;
 			supplier = fn;
 		}
-		
-		public Supplier<String> call(){
+
+		public Task(Supplier<String> fn) {
+			this(null, fn);
+		}
+
+		public Supplier<String> call() {
 			return supplier;
 		}
 	}
@@ -74,38 +142,64 @@ public class Tracker implements AutoCloseable {
 	 * port) and then adds itself to the Tracker's stack of awaiting clients
 	 */
 	class ClientConnection implements Runnable {
+
 		/*
-		 * summary of the client
+		 * The socket the client is talking to us through
 		 */
-		class ClientConnectionSummary {
-			@Expose
-			public String	host;
-			@Expose
-			public int		port;
+		protected final Socket				socket;
 
-			@Override
-			public String toString() {
-				return gson.toJson(this, ClientConnectionSummary.class);
-			}
-		}
+		/*
+		 * in for the socket
+		 */
+		protected final BufferedReader		in;
 
-		protected Socket					socket;
+		/*
+		 * out for the socket
+		 */
+		protected final DataOutputStream	out;
 
+		/*
+		 * The port the client is listening on
+		 */
 		@Expose
-		private ClientConnectionSummary	summary = null;
-		
-		public ClientConnectionSummary getSummary(){
-			if(summary == null){
-				throw new RuntimeException("Data not yet gathered from Client");
-			}
-			return summary;
-		}
+		private Integer						port		= null;
+
+		/*
+		 * the hostname
+		 */
+		@Expose
+		private String						hostname	= null;
 
 		/*
 		 * instantiate new ClientConnection, from a given socket
 		 */
 		public ClientConnection(Socket socket) {
 			this.socket = socket;
+			try {
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new DataOutputStream(socket.getOutputStream());
+				hostname = socket.getInetAddress().getHostAddress();
+			} catch (IOException ioe) {
+				throw new RuntimeException("Failed to open client connection", ioe);
+			}
+		}
+
+		/*
+		 * return the host the client is on
+		 */
+		public String hostname() {
+			if (hostname == null)
+				throw new NullPointerException();
+			return hostname;
+		}
+
+		/*
+		 * return the port the Client is listening on
+		 */
+		public int port() {
+			if (port == null)
+				throw new NullPointerException();
+			return port;
 		}
 
 		/*
@@ -115,42 +209,38 @@ public class Tracker implements AutoCloseable {
 		 */
 		@Override
 		public void run() {
-			// TODO
+			int port;
+			String msg;
+			try {
+				// It's listening for a single string, an int, the port that the
+				// client is listening on
+				msg = in.readLine();
+				port = Integer.parseInt(msg);
+			} catch (IOException ioe) {
+				throw new RuntimeException("Error reading client message", ioe);
+			} catch (RuntimeException re) {
+				throw new RuntimeException("Error parsing message from client", re);
+			}
+			this.port = port;
+			Tracker.this.clients.push(this);
 		}
 
 		@Override
 		public String toString() {
-			// TODO this is probably bad, should have a ClientConnectionSummary
-			// class for holding the json data
-			// it only needs to hold the clients IP and the port they're
-			// listening on
 			return gson.toJson(this, ClientConnection.class);
 		}
 	}
 
 	/*
-	 * path to config file TODO should read from argument instead
+	 * path to config file
 	 */
-	public static final String	CONFIG_PATH		= "tracker.json";
-	/*
-	 * Default configuration object
-	 */
-	public static final Config	DEFAULT_CONFIG;
-	/*
-	 * default port to connect to TODO move defaults to a .json file in the jar
-	 */
-	public static final int		DEFAULT_PORT	= 8000;
+	public static final String	CONFIG_PATH	= "tracker.json";
 
 	/*
 	 * GSON instance, used throughout the entire program GsonBuilder used, so in
 	 * order for Gson to use a field, it needs the @Expose annotation
 	 */
-	protected static Gson		gson			= new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-
-	static {
-		DEFAULT_CONFIG = new Config();
-		DEFAULT_CONFIG.port = DEFAULT_PORT;
-	}
+	protected static Gson		gson		= new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 
 	/*
 	 * main bruh
@@ -172,6 +262,7 @@ public class Tracker implements AutoCloseable {
 		try {
 			if (!configPath.toFile().exists()) {
 				// file does not exist, write default to path
+				config = Config.DEFAULT;
 				Files.write(configPath, gson.toJson(config).getBytes());
 			}
 			config = gson.fromJson(new String(Files.readAllBytes(configPath)), Config.class);
@@ -186,42 +277,42 @@ public class Tracker implements AutoCloseable {
 	/*
 	 * logger object, printing to System.in and System.out
 	 */
-	protected final ThreadLogger		logger			= new ThreadLogger(System.in, System.out);
+	protected final ThreadLogger		logger	= new ThreadLogger(System.in, System.out);
 
 	/*
 	 * running flag, threads use this to figure out when to turn off
 	 */
 	@Expose
-	private boolean						running			= true;
+	private boolean						running	= true;
 
 	/*
 	 * The currently waiting clients
 	 */
 	@Expose
-	protected Deque<ClientConnection>	clients			= new LinkedBlockingDeque<>();
+	protected Deque<ClientConnection>	clients	= new LinkedBlockingDeque<>();
 
 	/*
 	 * The trackers configuration
 	 */
 	@Expose
 	public final Config					config;
-
 	/*
 	 * the socket we're listening on
 	 */
 	@Expose
-	protected ServerSocket				socket			= null;
+	protected ServerSocket				socket	= null;
 
 	/*
 	 * Listens on the socket, waiting for a connection, passing those
 	 * connections to their own ClientConnectionThreads
 	 */
-	private final Thread listenThread;
+	private final Thread				listenThread;
+
 	/*
 	 * Waits for there to be two or more clients connected, then pops them, and
 	 * sends their connection info to each other
 	 */
-	private final Thread matchThread;
+	private final Thread				matchThread;
 
 	/*
 	 * Initialize new Tracker with given configuration
@@ -229,56 +320,20 @@ public class Tracker implements AutoCloseable {
 	public Tracker(Config config) {
 		this.config = config;
 		try {
-			socket = new ServerSocket();
+			socket = new ServerSocket(config.port);
 		} catch (IOException ioe) {
 			throw new RuntimeException("There was an error openning the socket", ioe);
 		}
 		// initialize local threads
-		matchThread = new Thread(()->{
-			int pairs = 0;
-			ClientConnection cc1, cc2;
-			TriFunction<ClientConnection, ClientConnection, Integer, Thread> pairClientsFactory = (conn1, conn2, count)->{
-				return new Thread(()->{
-					// TODO send the data for the two clients to each other
-					// NOTE this single thread needs to send conn1 to conn2 AND conn2 to conn1!!!
-				}, "PairThread#" + count);
-			};
-			logger.log("Starting to match...");
-			while(isRunning()){
-				if(clients.size() >= 2){
-					cc1 = clients.pop();
-					cc2 = clients.pop();
-					pairClientsFactory.apply(cc1, cc2, pairs).start();
-					pairs++;
-				}
-			}
-		}, "MatchThread");
-		
-		listenThread = new Thread(()->{
-			ClientConnection conn;
-			ClientConnectionThread cct;
-			Socket clientSocket;
-			logger.log("Starting to listen...");
-			while(isRunning()){
-				try{
-					clientSocket = socket.accept();
-				}
-				catch(IOException ioe){
-					ioe.printStackTrace();
-					continue;
-				}
-				// TODO refactor/reduce
-				conn = new ClientConnection(clientSocket);
-				cct = new ClientConnectionThread(conn);
-				cct.start();
-			}
-		}, "ListenThread");
+		matchThread = new MatchThread();
+		listenThread = new ListenThread();
 	}
 
 	/*
 	 * (non-Javadoc)
 	 *
-	 * @see java.lang.AutoCloseable#close()
+	 * @see java.lang.AutoCloseable#close() Close the Tracker, free it's
+	 * resources gracefully
 	 */
 	@Override
 	public void close() {
@@ -289,6 +344,9 @@ public class Tracker implements AutoCloseable {
 		}
 	}
 
+	/*
+	 * whether the Tracker is still running
+	 */
 	public boolean isRunning() {
 		return running;
 	}
@@ -308,17 +366,35 @@ public class Tracker implements AutoCloseable {
 			// an immutable map, but that'd have meant taking on a Guava
 			// dependency, and that seems silly for just one collection
 			Map<String, Task> tasks = new HashMap<>();
-			tasks.put("help", new Task("Display this helpscreen",()->""));
-			tasks.put("exit", new Task("Kill the tracker gracefully", ()->{
+			tasks.put("help",
+					new Task("Display this helpscreen",
+							() -> tasks.entrySet().stream()
+									.map((entry) -> entry.getKey() + "\t\t" + entry.getValue().docstring)
+									.reduce((String s1, String s2) -> s1 + "\n" + s2).get()));
+			tasks.put("exit", new Task("Kill the tracker gracefully", () -> {
 				stop();
 				return "Tracker stopped gracefully";
 			}));
-			tasks.put("list", new Task("List the current open connections", ()->""));	// TODO define list and kill using map and reduce
+			tasks.put("list", new Task("List the current open connections", () -> clients.stream()
+					.map(
+							(client) -> client.toString()).reduce(
+									(String s1, String s2) -> s1 + "\n" + s2).orElse("No connections")));
 			while (isRunning()) {
-				Task task = tasks.get(scan.nextLine());
+				String line;
+				try{
+					line = scan.nextLine();
+				}
+				catch(NoSuchElementException nsee){
+					System.out.println("Got EOF");
+					line = "exit";
+				}
+				if(line.equals(""))
+					continue;
+				Task task = tasks.get(line);
 				System.out.println(task == null ? "Sorry I don't know what that means" : task.call().get());
 			}
 		}
+
 	}
 
 	/*
@@ -331,7 +407,7 @@ public class Tracker implements AutoCloseable {
 		return gson.toJson(this, Tracker.class);
 	}
 
-	protected void stop() {
+	protected synchronized void stop() {
 		if (running) {
 			running = false;
 		} else
@@ -407,5 +483,28 @@ class ThreadLogger {
 
 	public void warn(String msg) {
 		println(MsgType.WARN, msg);
+	}
+}
+
+@FunctionalInterface
+interface TriFunction<A, B, C, R> {
+	public R apply(A a, B b, C c);
+}
+
+/*
+ * static methods that are useful
+ */
+class Util {
+	public static String concat(List<String> strings) {
+		return concat(strings, "");
+	}
+
+	public static String concat(List<String> strings, String delimeter) {
+		return strings.stream().reduce((s1, s2) -> s1 + delimeter + s2).get();
+	}
+
+	private Util() {
+		// we don't ever want to instantiate Util!
+		throw new RuntimeException();
 	}
 }
